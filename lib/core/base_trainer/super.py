1ba1ec1e408894adb40230ddda30f4792d1ab69c
liangzi <2120140200@mail.nankai.edu.cn>
2024-04-10 19:38:34 +0800

    fix bug

--- lib/core/base_trainer/super.py
+++ lib/core/base_trainer/super.py
@@ -1,8 +1,6 @@
 import math
 import sys
 
-
-
 sys.path.append('.')
 
 import timm
@@ -38,43 +36,38 @@ class Transform(nn.Module):
 
 
 class Modeleeg(nn.Module):
-    def __init__(self,):
+    def __init__(self, ):
         super(Modeleeg, self).__init__()
 
-        self.model=timm.create_model('efficientnet_b5',
-                                            pretrained=True,
-                                            in_chans=3,)
+        self.model = timm.create_model('efficientnet_b5',
+                                       pretrained=True,
+                                       in_chans=3, )
 
-
-
-        self.pool=nn.AdaptiveAvgPool2d(1)
-        self.fc=nn.Linear(2048,out_features=6,bias=True)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(2048, out_features=6, bias=True)
 
     def extract_features(self, x):
-
-
-        x=self.model.forward_features(x)
+        x = self.model.forward_features(x)
 
         return x
-    @torch.compile
+
     def forward(self, x):
         bs = x.size(0)
-        reshaped_tensor = x.view(bs,16,1000, 10)
+        reshaped_tensor = x.view(bs, 16, 1000, 10)
 
-        reshaped_and_permuted_tensor = reshaped_tensor.permute(0,1,3,2)
+        reshaped_and_permuted_tensor = reshaped_tensor.permute(0, 1, 3, 2)
 
-        reshaped_and_permuted_tensor= reshaped_and_permuted_tensor.reshape(bs,16*10,1000)
+        reshaped_and_permuted_tensor = reshaped_and_permuted_tensor.reshape(bs, 16 * 10, 1000)
 
-        x=torch.unsqueeze(reshaped_and_permuted_tensor,dim=1)
+        x = torch.unsqueeze(reshaped_and_permuted_tensor, dim=1)
 
-        x=torch.cat([x,x,x],dim=1)
-        bs=x.size(0)
-
+        x = torch.cat([x, x, x], dim=1)
+        bs = x.size(0)
 
         x = self.extract_features(x)
 
         x = self.pool(x)
-        x = x.view(bs,-1)
+        x = x.view(bs, -1)
 
         return x
 
@@ -96,9 +89,6 @@ class Modelspec(nn.Module):
 
         self.avg = nn.AdaptiveAvgPool2d(1)
 
-
-
-    # @torch.compile
     def forward(self, x):
         x = torch.unsqueeze(x, dim=1)
         x = torch.cat([x, x, x], dim=1)
@@ -115,27 +105,25 @@ class Net(nn.Module):
     def __init__(self, num_classes=1):
         super().__init__()
 
-
-        self.transform=Transform()
+        self.transform = Transform()
         self.model_wave = Modeleeg()
 
         self.model_spec = Modelspec()
 
         self.pool = nn.AdaptiveAvgPool3d(1)
-        self.fc = nn.Linear(2048*2, 6, bias=True)
+        self.fc = nn.Linear(2048 * 2, 6, bias=True)
 
         self.droup = nn.Dropout(0.5)
 
-
-    def forward(self,  eeg):
+    def forward(self, eeg):
         # do preprocess
         bs = eeg.size(0)
 
-        eeg_spec=self.transform(eeg)
+        eeg_spec = self.transform(eeg)
 
         x = self.model_wave(eeg)
         y = self.model_spec(eeg_spec)
-        x = torch.cat([x,y],dim=1)
+        x = torch.cat([x, y], dim=1)
 
         x = self.droup(x)
         x = self.fc(x)
@@ -149,6 +137,7 @@ if __name__ == '__main__':
     from thop import profile
 
     from thop import clever_format
+
     dummy_data = torch.randn(1, 4, 100, 300, device='cpu')
     dummy_waves = torch.randn(1, 16, 10000, device='cpu')
     model = Net()
@@ -157,4 +146,4 @@ if __name__ == '__main__':
     macs, params = profile(model, inputs=[dummy_waves])
     macs, params = clever_format([macs, params], "%.3f")
 
-    print(macs,params)
+    print(macs, params)
